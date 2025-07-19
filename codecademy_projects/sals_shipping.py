8e0bf1837c46f0dfd1e48040ef0dd920da76ae44
Ana Kuridze <anakuridze49@gmail.com>
2025-07-19 13:01:10 -0400

    Create sals_shipping.py

---
+++ codecademy_projects/sals_shipping.py
@@ -0,0 +1,28 @@
+weight = 41.5
+
+#Ground shipping
+if weight <= 2:
+  cost_ground = weight * 1.50 + 20
+elif weight <= 6:
+  cost_ground = weight * 3 + 20
+elif weight <= 10:
+  cost_ground = weight * 4 + 20
+else: 
+  cost_ground = weight * 4.75 + 20
+
+print(cost_ground)
+
+premium_ground_shipping = 125.00
+print(premium_ground_shipping)
+
+#DRONE SHIPPING
+
+if weight <=2:
+  cost_drone = weight * 4.50 
+elif weight <6:
+  cost_drone = weight * 9.00
+elif weight <10: 
+  cost_drone = weight * 12.00
+else: 
+  cost_drone = weight * 14.25
+print(cost_drone)
